b4e6dedfbcea0d824f78b515da1d0d5738529d87
YaIsrak <israkdreamstar@gmail.com>
2026-01-05 20:09:28 +0600

    category page

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from blogs.models import Blog
+from blogs.models import Category
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+def posts_by_category(request, category_id):
+    posts = Blog.objects.filter(status='Published', category=category_id)
+    category = get_object_or_404(Category, pk=category_id)
+
+    context = {
+        'posts': posts,
+        'category': category,
+    }
+    return render(request, 'posts_by_category.html', context)
