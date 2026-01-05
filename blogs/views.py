e544ade508e7326120c1b48854f908e84aa2b28f
YaIsrak <israkdreamstar@gmail.com>
2026-01-05 20:21:05 +0600

    single blog post

--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from blogs.models import Blog
-from blogs.models import Category
+from blogs.models import Blog, Comment, Category
 from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def posts_by_category(request, category_id):
@@ -13,3 +13,24 @@ def posts_by_category(request, category_id):
         'category': category,
     }
     return render(request, 'posts_by_category.html', context)
+
+def blogs(request, slug):
+    single_blog = get_object_or_404(Blog, slug=slug, status='Published')
+    if request.method == 'POST':
+        comment = Comment()
+        comment.user = request.user
+        comment.blog = single_blog
+        comment.comment = request.POST['comment']
+        comment.save()
+        return HttpResponseRedirect(request.path_info)
+
+    # Comments
+    comments = Comment.objects.filter(blog=single_blog)
+    comment_count = comments.count()
+
+    context = {
+        'single_blog': single_blog,
+        'comments': comments,
+        'comment_count': comment_count,
+    }
+    return render(request, 'blogs.html', context)
